bb34dba88c84b956ff5c5723b7c884922749b735
MThomasCox <mthomasc@gmail.com>
2025-10-02 22:04:13 -0400

    dark mode added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-// Scroll-to-top tool
 document.addEventListener("DOMContentLoaded", function () {
-  const btn = document.querySelector("#toTopBtn");
-  if (btn) {
-    btn.addEventListener("click", () => {
+  // Scroll-to-top button
+  const toTopBtn = document.querySelector("#toTopBtn");
+  if (toTopBtn) {
+    toTopBtn.addEventListener("click", () => {
       window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+  }
+
+  // Dark Mode Toggle
+  const darkToggle = document.querySelector("#darkModeToggle");
+  if (darkToggle) {
+    darkToggle.addEventListener("click", () => {
+      document.body.classList.toggle("dark-mode");
     });
   }
 });
